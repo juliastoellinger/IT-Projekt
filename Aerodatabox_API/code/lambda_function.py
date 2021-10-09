fc98f215ee06ce4f01e3981e2860cc24f03e3cbf
tobiasleitner5 <tobiasleitner5@gmail.com>
2021-10-09 16:50:31 +0200

    first source finished

---
+++ Aerodatabox_API/code/lambda_function.py
@@ -0,0 +1,33 @@
+import json
+import http.client
+import boto3
+from datetime import datetime, timedelta
+import dateutil.tz
+
+def lambda_handler(event, context):
+    client = boto3.resource("dynamodb")
+    table = client.Table('flights')
+    berlin = dateutil.tz.gettz('Europe/Berlin')
+    now = datetime.now(berlin)
+    data = get_data(now)
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    response = table.put_item(Item={'date': dt_string, 'data': data})
+    return response
+
+    
+def get_data(now):
+    conn = http.client.HTTPSConnection("aerodatabox.p.rapidapi.com")
+
+    headers = {
+        'x-rapidapi-host': "aerodatabox.p.rapidapi.com",
+        'x-rapidapi-key': "81e645acd6msh213095fe1eb2a21p17566ejsn75c5a29c2394"
+    }
+    
+    d = now - timedelta(hours=2, minutes=00)
+    from_ = d.strftime("%Y-%m-%dT%H:%M")
+    to_ = now.strftime("%Y-%m-%dT%H:%M")
+    conn.request("GET", f"/flights/airports/icao/EHAM/{from_}/{to_}?withLeg=true&withCancelled=true&withCodeshared=true&withCargo=true&withPrivate=true&withLocation=false", headers=headers)
+    res = conn.getresponse()
+    data = res.read().decode('utf8')
+    return data
+
