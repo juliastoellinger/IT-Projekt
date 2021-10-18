b6dc2711d9a34698399f27694eddd182b68d9aeb
juliastoellinger <80411742+juliastoellinger@users.noreply.github.com>
2021-10-19 01:36:53 +0200

    Detailansicht pro Flugnummer wird angezeigt

--- FlightAware/main.py
+++ FlightAware/main.py
@@ -3,18 +3,40 @@ import html_to_json
 import requests
 from bs4 import BeautifulSoup
 
+#Von der Flightaware-Seite wird der HTML Code als Variable dargestellt
 result = requests.get("https://de.flightaware.com/live/airport/EDDF/departures")
 src = result.content
 
 soup = BeautifulSoup(src)
 
-body = soup.find('<table class="prettyTable fullWidth" style="background-color: white;">')
 
 
+for link in soup.findAll('a'):
+    #print(link.get('href'))
+    a = str(link.get('href'))
+    # Für jeden Link, der auf die Flugnummer zeigt, werden die geplante Ankuftszeit und die tatsächliche Ankunftszeit angegeben
+    if 'live/flight/id' in a :
+        #print(a)
+        detail = 'https://de.flightaware.com/' + a
+        detailResult = requests.get(detail)
+        detailSrc = detailResult.content
 
+        detailSoup = BeautifulSoup(detailSrc)
+        #print(detailSoup)
+        detailString = str(detailSoup)
+        pos1 = detailString.find('flightPageDataTimesParent', 0)
+        pos2 = detailString.find('flightPageDataTableContainer', 0)
+        detailWithoutTop = detailString[pos1:]
+        detailX = detailWithoutTop[:pos2]
+        print(detailX)
+        print(a)
+
+
+#Von der Übersichts-Seite wird der HTML Code der Tabelle eingelesen und in ein JSON umgewandelt
 text = str(soup)
 
 posStart = text.find('<table class="prettyTable fullWidth" style="background-color: white;">', 0)
+
 
 withoutTop = text[posStart:]
 
@@ -25,4 +47,4 @@ html_string = """<head>
     <title>Test site</title>
     <meta charset="UTF-8"></head>"""
 output_json = html_to_json.convert(html)
-print(output_json)
+#print(output_json)
