af3673aab8efd5b0c327358641aa6c0fb3fa3877
juliastoellinger <80411742+juliastoellinger@users.noreply.github.com>
2021-11-23 08:58:32 +0100

    Update extract_attributes_and_cleaning.py

--- Flightradar24/code/extract_attributes_and_cleaning.py
+++ Flightradar24/code/extract_attributes_and_cleaning.py
@@ -1,11 +1,12 @@
 import boto3
 import json
+import time
 
 if __name__ == '__main__':
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('flights2')
     data = []
-    data.append(f"number;date;scheduled_departure_time;real_departure;estimated_departure")
+    data.append(f"number;scheduledTime;actualTime")
     json_decode_error_counter = 0
 
     response = table.scan()
@@ -17,6 +18,7 @@ if __name__ == '__main__':
         try:
             flights = json.loads(string)
             for flight in flights:
+                #print(flight)
                 #if real and estimated departure time is 'None' we have no basis on which we could caluclate a difference to the scheduled departure time --> sort out
                 try:
                     flight['flight']['identification']['number']['default']
@@ -24,57 +26,75 @@ if __name__ == '__main__':
                     flight['flight']['time']['scheduled']['departure_time']
                 except KeyError:
                     pass
-                if (flight['flight']['time']['real']['departure'] == 'None' and flight['flight']['time']['estimated']['departure'] == 'None') or flight['flight']['time']['scheduled']['departure_date'] == 'None':
+                if (flight['flight']['time']['real']['departure'] == 'None' and flight['flight']['airline']=='None' and flight['flight']['time']['estimated']['departure'] == 'None') or flight['flight']['time']['scheduled']['departure_date'] == 'None':
                     pass
                 else:
                     try:
-                        data.append(f"{flight['flight']['identification']['number']['default']};{flight['flight']['time']['scheduled']['departure_date']};{flight['flight']['time']['scheduled']['departure_time']};{flight['flight']['time']['real']['departure']};{flight['flight']['time']['estimated']['departure_time']}")
+                        scheduledTimeUTC = time.mktime(time.strptime(flight['flight']['time']['scheduled']['departure_date']+flight['flight']['time']['scheduled']['departure_time'], "%Y%m%d%H%M"))
+                        actualTimeUTC = time.mktime(time.strptime(flight['flight']['time']['scheduled']['departure_date']+flight['flight']['time']['estimated']['departure_time'], "%Y%m%d%H%M"))
+                        airline = ""
+                        if (flight['flight']['airline']=='None'):
+                            airline = 'None'
+                        else:
+                            airline = flight['flight']['airline']['name']
+                        data.append(f"{airline}+{flight['flight']['identification']['number']['default']};{scheduledTimeUTC};{actualTimeUTC}")
+
                     except KeyError:
                         try:
-                            data.append(f"{flight['flight']['identification']['number']['default']};{flight['flight']['time']['scheduled']['departure_date']};{flight['flight']['time']['scheduled']['departure_time']};{flight['flight']['time']['real']['departure_time']};None")
+                            data.append(f"{airline}+{flight['flight']['identification']['number']['default']};{scheduledTimeUTC};{actualTimeUTC}")
                         except KeyError:
                             pass
         except json.decoder.JSONDecodeError:
             json_decode_error_counter = json_decode_error_counter+1
             pass
 
-    # because of the "pagination" we need to iterate over the pages as long as there is no page left
-    while 'LastEvaluatedKey' in response:
-        response = table.scan(
-            ExclusiveStartKey=response['LastEvaluatedKey']
-        )
-        for item in response['Items']:
-            string = item['data']
-            string = string.replace("\'", "\"")
-            string = string.replace("True", "\"True\"")
-            string = string.replace("False", "\"False\"")
-            try:
-                flights = json.loads(string)
-                for flight in flights:
-                    # if real and estimated departure time is 'None' we have no basis on which we could caluclate a difference to the scheduled departure time --> sort out
-                    try:
-                        flight['flight']['identification']['number']['default']
-                        flight['flight']['time']['scheduled']['departure_date']
-                        flight['flight']['time']['scheduled']['departure_time']
-                    except KeyError:
-                        pass
-                    if (flight['flight']['time']['real']['departure'] == 'None' and
-                        flight['flight']['time']['estimated']['departure'] == 'None') or \
-                            flight['flight']['time']['scheduled']['departure_date'] == 'None':
-                        pass
-                    else:
+        # because of the "pagination" we need to iterate over the pages as long as there is no page left
+        while 'LastEvaluatedKey' in response:
+            response = table.scan(
+                ExclusiveStartKey=response['LastEvaluatedKey']
+            )
+            for item in response['Items']:
+                string = item['data']
+                string = string.replace("\'", "\"")
+                string = string.replace("True", "\"True\"")
+                string = string.replace("False", "\"False\"")
+                try:
+                    flights = json.loads(string)
+                    for flight in flights:
+                        # if real and estimated departure time is 'None' we have no basis on which we could caluclate a difference to the scheduled departure time --> sort out
                         try:
-                            data.append(
-                                f"{flight['flight']['identification']['number']['default']};{flight['flight']['time']['scheduled']['departure_date']};{flight['flight']['time']['scheduled']['departure_time']};{flight['flight']['time']['real']['departure']};{flight['flight']['time']['estimated']['departure_time']}")
+                            flight['flight']['identification']['number']['default']
+                            flight['flight']['time']['scheduled']['departure_date']
+                            flight['flight']['time']['scheduled']['departure_time']
                         except KeyError:
+                            pass
+                        if (flight['flight']['time']['real']['departure'] == 'None' and
+                            flight['flight']['time']['estimated']['departure'] == 'None') or \
+                                flight['flight']['time']['scheduled']['departure_date'] == 'None':
+                            pass
+                        else:
                             try:
-                                data.append(
-                                    f"{flight['flight']['identification']['number']['default']};{flight['flight']['time']['scheduled']['departure_date']};{flight['flight']['time']['scheduled']['departure_time']};{flight['flight']['time']['real']['departure_time']};None")
+                                scheduledTimeUTC = time.mktime(time.strptime(
+                                    flight['flight']['time']['scheduled']['departure_date'] +
+                                    flight['flight']['time']['scheduled']['departure_time'], "%Y%m%d%H%M"))
+                                actualTimeUTC = time.mktime(time.strptime(
+                                    flight['flight']['time']['scheduled']['departure_date'] +
+                                    flight['flight']['time']['estimated']['departure_time'], "%Y%m%d%H%M"))
+                                airline = ""
+                                if (flight['flight']['airline'] == 'None'):
+                                    airline = 'None'
+                                else:
+                                    airline = flight['flight']['airline']['name']
+                                data.append(f"{airline}+{flight['flight']['identification']['number']['default']};{scheduledTimeUTC};{actualTimeUTC}")
                             except KeyError:
-                                pass
-            except json.decoder.JSONDecodeError:
-                json_decode_error_counter = json_decode_error_counter + 1
-                pass
+                                try:
+                                    data.append(
+                                        f"{airline}+{flight['flight']['identification']['number']['default']};{scheduledTimeUTC};{actualTimeUTC}")
+                                except KeyError:
+                                    pass
+                except json.decoder.JSONDecodeError:
+                    json_decode_error_counter = json_decode_error_counter + 1
+                    pass
 
     outF = open("myOutFile.csv", "w")
     outF.write("\n")
