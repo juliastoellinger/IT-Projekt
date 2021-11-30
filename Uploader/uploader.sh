00fc8e5e8d71f05df971693491ac1a6ccf3dc70b
tobiasleitner5 <tobiasleitner5@gmail.com>
2021-11-30 13:40:27 +0100

    added uploader files

--- Uploader/uploader.sh
+++ Uploader/uploader.sh
@@ -1,10 +1,11 @@
-i=1
-e=68
+i=0
+e=108
 
-file=data_
+file=data/aerodata_flughafen_amsterdam_
 
 i=1
 while [ $i -lt $e ]; do 
     realfile=$file$i.json && i=$[$i+1];
     curl -H "Content-Type: application/json" -d "$(< $realfile )" -X POST http://localhost:4000/api/data
+    echo $realfile uploaded.
 done
